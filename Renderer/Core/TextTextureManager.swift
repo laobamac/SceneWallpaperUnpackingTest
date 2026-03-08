9981cab655c668472801431d436868f605a24efc
laobamac <wxcznb@qq.com>
2026-03-08 18:22:20 +0800

    111

--- Renderer/Core/TextTextureManager.swift
+++ Renderer/Core/TextTextureManager.swift
@@ -5,29 +5,28 @@
 //  Created by laobamac on 2026/3/8.
 //
 
-import Metal
 import AppKit
-import CoreText
 import CoreGraphics
+import CoreText
+import Metal
 
 class TextTextureManager {
     static let shared = TextTextureManager()
     private var registeredFonts: [String: NSFont] = [:]
     private var device: MTLDevice?
-
     func setup(device: MTLDevice) {
         self.device = device
     }
-
     func registerFont(path: String, baseURL: URL) {
         let fontURL = baseURL.appendingPathComponent(path)
-        guard FileManager.default.fileExists(atPath: fontURL.path) else { return }
-        
+        guard FileManager.default.fileExists(atPath: fontURL.path) else {
+            return
+        }
         var error: Unmanaged<CFError>?
         CTFontManagerRegisterFontsForURL(fontURL as CFURL, .process, &error)
-        
         if let dataProvider = CGDataProvider(url: fontURL as CFURL),
-           let cgFont = CGFont(dataProvider) {
+            let cgFont = CGFont(dataProvider)
+        {
             if let postScriptName = cgFont.postScriptName as String? {
                 if let font = NSFont(name: postScriptName, size: 12) {
                     registeredFonts[path] = font
@@ -35,33 +34,24 @@ class TextTextureManager {
             }
         }
     }
-
-    func createTexture(text: String, fontPath: String?, fontSize: Float, size: SIMD2<Float>, horizontalAlign: String?, verticalAlign: String?, padding: Float) -> MTLTexture? {
+    func createTexture(
+        text: String,
+        fontPath: String?,
+        fontSize: Float,
+        size: SIMD2<Float>,
+        horizontalAlign: String?,
+        verticalAlign: String?,
+        padding: Float,
+        limitWidth: Bool?
+    ) -> MTLTexture? {
         guard let device = device else { return nil }
-        
         let weFontScale: CGFloat = 2.8
         let backingScale: CGFloat = 2.0
-        
-        let width = max(1, Int(CGFloat(size.x) * backingScale))
-        let height = max(1, Int(CGFloat(size.y) * backingScale))
-        
         let adjustedFontSize = CGFloat(fontSize) * weFontScale * backingScale
         let scaledPadding = CGFloat(padding) * backingScale
-
+        let baseWidth = CGFloat(size.x) * backingScale
+        let baseHeight = CGFloat(size.y) * backingScale
         let colorSpace = CGColorSpaceCreateDeviceRGB()
-        let bytesPerPixel = 4
-        let bytesPerRow = bytesPerPixel * width
-        
-        guard let context = CGContext(data: nil,
-                                      width: width,
-                                      height: height,
-                                      bitsPerComponent: 8,
-                                      bytesPerRow: bytesPerRow,
-                                      space: colorSpace,
-                                      bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue) else { return nil }
-
-        context.clear(CGRect(x: 0, y: 0, width: width, height: height))
-        
         var font = NSFont.systemFont(ofSize: adjustedFontSize)
         if let fontPath = fontPath {
             if let customFont = registeredFonts[fontPath] {
@@ -72,56 +62,86 @@ class TextTextureManager {
                 }
             }
         }
-
         let paragraphStyle = NSMutableParagraphStyle()
         paragraphStyle.lineSpacing = 0
-        paragraphStyle.paragraphSpacing = 0
-        paragraphStyle.lineHeightMultiple = 1.0
-        paragraphStyle.maximumLineHeight = adjustedFontSize
-        paragraphStyle.minimumLineHeight = adjustedFontSize
-        
         switch horizontalAlign {
         case "left": paragraphStyle.alignment = .left
         case "right": paragraphStyle.alignment = .right
         case "center": paragraphStyle.alignment = .center
         default: paragraphStyle.alignment = .left
         }
-
         let attributes: [NSAttributedString.Key: Any] = [
             .font: font,
             .foregroundColor: NSColor.white,
             .paragraphStyle: paragraphStyle,
-            .baselineOffset: 0
         ]
-
-        let attributedString = NSAttributedString(string: text, attributes: attributes)
-        let drawingWidth = max(1, CGFloat(width) - scaledPadding * 2)
-        let options: NSString.DrawingOptions = [.usesLineFragmentOrigin, .usesFontLeading]
-        let textRect = attributedString.boundingRect(with: CGSize(width: drawingWidth, height: .greatestFiniteMagnitude), options: options)
-
-        let drawX = scaledPadding
-        var drawY: CGFloat = 0
-
-        switch verticalAlign {
-        case "top":
-            drawY = CGFloat(height) - textRect.height - scaledPadding
-        case "bottom":
-            drawY = scaledPadding
-        case "center":
-            drawY = (CGFloat(height) - textRect.height) / 2.0
-        default:
-            drawY = CGFloat(height) - textRect.height - scaledPadding
+        let attributedString = NSAttributedString(
+            string: text,
+            attributes: attributes
+        )
+        let isLimited = limitWidth ?? false
+        let drawingWidth =
+            isLimited ? max(1, baseWidth - scaledPadding * 2) : 100000.0
+        let options: NSString.DrawingOptions = [
+            .usesLineFragmentOrigin, .usesFontLeading,
+        ]
+        let textRect = attributedString.boundingRect(
+            with: CGSize(width: drawingWidth, height: .greatestFiniteMagnitude),
+            options: options
+        )
+        let finalWidth = max(
+            baseWidth,
+            ceil(textRect.width + scaledPadding * 2)
+        )
+        let finalHeight = max(
+            baseHeight,
+            ceil(textRect.height + scaledPadding * 2)
+        )
+        let width = max(1, Int(finalWidth))
+        let height = max(1, Int(finalHeight))
+        let bytesPerPixel = 4
+        let bytesPerRow = bytesPerPixel * width
+        guard
+            let context = CGContext(
+                data: nil,
+                width: width,
+                height: height,
+                bitsPerComponent: 8,
+                bytesPerRow: bytesPerRow,
+                space: colorSpace,
+                bitmapInfo: CGImageAlphaInfo.premultipliedLast.rawValue
+            )
+        else { return nil }
+        context.clear(CGRect(x: 0, y: 0, width: width, height: height))
+        let drawX: CGFloat
+        if isLimited {
+            drawX = scaledPadding
+        } else {
+            switch horizontalAlign {
+            case "center": drawX = (finalWidth - textRect.width) / 2.0
+            case "right": drawX = finalWidth - textRect.width - scaledPadding
+            default: drawX = scaledPadding
+            }
         }
-
+        let drawY: CGFloat
+        switch verticalAlign {
+        case "top": drawY = CGFloat(height) - textRect.height - scaledPadding
+        case "bottom": drawY = scaledPadding
+        case "center": drawY = (CGFloat(height) - textRect.height) / 2.0
+        default: drawY = CGFloat(height) - textRect.height - scaledPadding
+        }
         NSGraphicsContext.saveGraphicsState()
         let nsContext = NSGraphicsContext(cgContext: context, flipped: false)
         NSGraphicsContext.current = nsContext
-        
-        let targetRect = CGRect(x: drawX, y: drawY, width: drawingWidth, height: textRect.height)
+        let targetRect = CGRect(
+            x: drawX,
+            y: drawY,
+            width: isLimited
+                ? (baseWidth - scaledPadding * 2) : (textRect.width + 1),
+            height: textRect.height
+        )
         attributedString.draw(with: targetRect, options: options)
-        
         NSGraphicsContext.restoreGraphicsState()
-
         let textureDescriptor = MTLTextureDescriptor()
         textureDescriptor.pixelFormat = .rgba8Unorm
         textureDescriptor.width = width
@@ -129,18 +149,18 @@ class TextTextureManager {
         textureDescriptor.textureType = .type2DArray
         textureDescriptor.arrayLength = 1
         textureDescriptor.usage = [.shaderRead]
-        
-        guard let texture = device.makeTexture(descriptor: textureDescriptor) else { return nil }
-
+        guard let texture = device.makeTexture(descriptor: textureDescriptor)
+        else { return nil }
         if let data = context.data {
-            texture.replace(region: MTLRegionMake2D(0, 0, width, height),
-                            mipmapLevel: 0,
-                            slice: 0,
-                            withBytes: data,
-                            bytesPerRow: bytesPerRow,
-                            bytesPerImage: bytesPerRow * height)
+            texture.replace(
+                region: MTLRegionMake2D(0, 0, width, height),
+                mipmapLevel: 0,
+                slice: 0,
+                withBytes: data,
+                bytesPerRow: bytesPerRow,
+                bytesPerImage: bytesPerRow * height
+            )
         }
-
         return texture
     }
 }
--- Renderer/Objects/TextRenderable.swift
+++ Renderer/Objects/TextRenderable.swift
@@ -5,9 +5,9 @@
 //  Created by laobamac on 2026/3/8.
 //
 
+import Foundation
 import MetalKit
 import simd
-import Foundation
 
 class TextRenderable: RenderableObject {
     let sceneObject: SceneObject
@@ -15,7 +15,6 @@ class TextRenderable: RenderableObject {
     var currentText: String = ""
     var scriptEngine: ScriptEngine?
     var originScriptEngine: ScriptEngine?
-    
     init?(
         sceneObject: SceneObject,
         textureManager: TextTextureManager,
@@ -26,14 +25,28 @@ class TextRenderable: RenderableObject {
     ) {
         self.sceneObject = sceneObject
         self.textTextureManager = textureManager
-        
-        let (pos, rotation, size, scale) = RenderableObject.parseTransforms(sceneObject)
-        
-        let desc = MTLTextureDescriptor.texture2DDescriptor(pixelFormat: .rgba8Unorm, width: 1, height: 1, mipmapped: false)
-        guard let dummyTexture = device.makeTexture(descriptor: desc) else { return nil }
-        
-        super.init(position: pos, rotation: rotation, size: size, scale: scale, alpha: sceneObject.alpha ?? 1.0, texture: dummyTexture, pipeline: pipeline, depthState: depthState)
-        
+        let (pos, rotation, size, scale) = RenderableObject.parseTransforms(
+            sceneObject
+        )
+        let desc = MTLTextureDescriptor.texture2DDescriptor(
+            pixelFormat: .rgba8Unorm,
+            width: 1,
+            height: 1,
+            mipmapped: false
+        )
+        guard let dummyTexture = device.makeTexture(descriptor: desc) else {
+            return nil
+        }
+        super.init(
+            position: pos,
+            rotation: rotation,
+            size: size,
+            scale: scale,
+            alpha: sceneObject.alpha ?? 1.0,
+            texture: dummyTexture,
+            pipeline: pipeline,
+            depthState: depthState
+        )
         if let textData = sceneObject.text {
             switch textData {
             case .string(let str):
@@ -41,49 +54,75 @@ class TextRenderable: RenderableObject {
             case .object(let obj):
                 self.currentText = obj.value ?? ""
                 if let script = obj.script {
-                    self.scriptEngine = ScriptEngine(script: script, properties: obj.scriptproperties, canvasSize: canvasSize)
+                    self.scriptEngine = ScriptEngine(
+                        script: script,
+                        properties: obj.scriptproperties,
+                        canvasSize: canvasSize
+                    )
                 }
             }
         }
-        
-        if let originObj = sceneObject.origin, case .complex(let script, _, let props) = originObj, let s = script {
-            self.originScriptEngine = ScriptEngine(script: s, properties: props, canvasSize: canvasSize)
+        if let originObj = sceneObject.origin,
+            case .complex(let script, _, let props) = originObj, let s = script
+        {
+            self.originScriptEngine = ScriptEngine(
+                script: s,
+                properties: props,
+                canvasSize: canvasSize
+            )
         }
-        
         updateTexture()
     }
-    
     override func update(commandBuffer: MTLCommandBuffer) {
         var needsUpdate = false
-        
         if let originEngine = originScriptEngine {
-            if let newPos = originEngine.evaluateUpdate(value: self.localPosition) as? SIMD3<Float> {
+            if let newPos = originEngine.evaluateUpdate(
+                value: self.localPosition
+            ) as? SIMD3<Float> {
                 if newPos != self.localPosition {
                     self.localPosition = newPos
                 }
             }
         }
-        
         if let textEngine = scriptEngine {
-            if let updatedText = textEngine.evaluateUpdate(value: self.currentText) as? String {
+            if let updatedText = textEngine.evaluateUpdate(
+                value: self.currentText
+            ) as? String {
                 if updatedText != self.currentText {
                     self.currentText = updatedText
                     needsUpdate = true
                 }
             }
         }
-        
         if needsUpdate {
             updateTexture()
         }
     }
-    
     private func updateTexture() {
         let fontSize = sceneObject.pointsize ?? 32.0
         let padding = sceneObject.padding ?? 0.0
-        
-        if let newTexture = textTextureManager.createTexture(text: currentText, fontPath: sceneObject.font, fontSize: fontSize, size: self.size, horizontalAlign: sceneObject.horizontalalign, verticalAlign: sceneObject.verticalalign, padding: padding) {
+        let limitWidth = sceneObject.limitwidth
+        let referenceSize: SIMD2<Float>
+        if let s = sceneObject.size {
+            referenceSize = s.float2Value
+        } else {
+            referenceSize = SIMD2<Float>(400, 400)
+        }
+        if let newTexture = textTextureManager.createTexture(
+            text: currentText,
+            fontPath: sceneObject.font,
+            fontSize: fontSize,
+            size: referenceSize,
+            horizontalAlign: sceneObject.horizontalalign,
+            verticalAlign: sceneObject.verticalalign,
+            padding: padding,
+            limitWidth: limitWidth
+        ) {
             self.texture = newTexture
+            let backingScale: Float = 2.0
+            self.size =
+                SIMD2<Float>(Float(newTexture.width), Float(newTexture.height))
+                / backingScale
         }
     }
 }
