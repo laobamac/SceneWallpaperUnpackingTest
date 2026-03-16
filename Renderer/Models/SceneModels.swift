07bf586949142c09b3ed88b4166eee68ad8de41a
laobamac <wxcznb@qq.com>
2026-03-16 23:37:51 +0800

    1

--- Renderer/Models/SceneModels.swift
+++ Renderer/Models/SceneModels.swift
@@ -42,7 +42,6 @@ struct SceneObject: Codable {
     let name: String?
     let image: String?
     let type: String?
-    let particle: String?
     let origin: ScriptableValue?
     let size: ScriptableValue?
     let scale: ScriptableValue?
@@ -63,7 +62,6 @@ struct SceneObject: Codable {
         case name
         case image
         case type
-        case particle
         case origin
         case size
         case scale
